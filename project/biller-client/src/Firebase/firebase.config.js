122988d959f08f01b2c49c3cc2f31e96fda1396e
gazinafis007 <gazinafisrafi.gnr@gmail.com>
2023-01-29 02:43:07 +0600

    env added

--- project/biller-client/src/Firebase/firebase.config.js
+++ project/biller-client/src/Firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-  apiKey: "AIzaSyADWfqT2bysGuQAJSwlpCnSfOrHes2WxOw",
-  authDomain: "biller-client.firebaseapp.com",
-  projectId: "biller-client",
-  storageBucket: "biller-client.appspot.com",
-  messagingSenderId: "916386914201",
-  appId: "1:916386914201:web:0249e40d27866a95fafd08",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
